952760e4e56fbb5d60c4f4b640837cf6dac438ae
jarrett leland <lelandjarrett@gmail.com>
2022-06-09 09:41:48 -0400

    delete some items

--- POC/final/Models/AzureStorageConfig.cs
+++
@@ -1,8 +0,0 @@
-namespace FileUploader.Models
-{
-    public class AzureStorageConfig
-    {
-        public string ConnectionString { get; set; }
-        public string FileContainerName { get; set; }
-    }
-}
